da741b7020fab5be770accb04f8c1cdc4263f135
bitmotte <bitmotte@proton.me>
2026-03-07 20:33:14 -0500

    destroy on slam

--- MBallPlugin/patches/MaliciousCorpse.cs
+++ MBallPlugin/patches/MaliciousCorpse.cs
@@ -6,10 +6,14 @@ public class MaliciousCorpse : EnemyScript
 {
     public int test = 0;
     public EnemyIdentifier eid;
+    public Enemy en;
+    public Machine mach;
 
     public void Awake()
     {
         eid = GetComponent<EnemyIdentifier>();
+        en = GetComponent<Enemy>();
+        mach = GetComponent<Machine>();
         UnityEvent deathEvent = new();
         deathEvent.AddListener(Break);
 
@@ -18,7 +22,7 @@ public class MaliciousCorpse : EnemyScript
 
     public override void OnDamage(ref DamageData data)
     {
-        if(eid.health <= 0)
+        if(eid.health <= 0 || data.hitter == "ground slam")
         {
             Break();
         }
@@ -26,6 +30,11 @@ public class MaliciousCorpse : EnemyScript
 
     public void Break()
     {
+        eid.health = 0;
+        en.health = 0;
+        mach.health = 0;
+        eid.ForceGetHealth();
+
         gameObject.transform.GetChild(0).GetChild(0).GetChild(1).gameObject.SetActive(false);
         foreach (Collider collider in GetComponentsInChildren<Collider>())
         {
